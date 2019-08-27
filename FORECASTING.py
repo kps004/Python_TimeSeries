f53929ed4d8ae028f8985fc96fd43c093d9539c2
kps004 <36299014+kps004@users.noreply.github.com>
2019-08-27 21:15:49 +0530

    Update FORECASTING.py

--- FORECASTING.py
+++ FORECASTING.py
@@ -1,56 +1,22 @@
-
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
-from sklearn.ensemble.forest import RandomForestRegressor
+import datetime as datetime
+import xlsxwriter
+get_ipython().run_line_magic('matplotlib', 'inline')
+
 
 # In[2]:
 
 
-df = pd.read_csv("Book2.csv")
-
-
-# In[3]:
-
-
-
-count = df["LCLid"].count()
-
-
-# In[4]:
-
-
-li = [1]
-for x in range(count):
-     li.append(x)
-   
-
-
-# In[5]:
-
-
-colu = {"key": li}
-dd = pd.DataFrame(colu)
-
-df["index"] = dd.copy()
-lclib  = df.groupby(by = "LCLid")
-A = lclib.get_group("MAC000002")
-
-
-# In[6]:
-
-
-lclid = df.groupby(by = "LCLid")
-a = lclid.get_group("MAC000002")
-c = a["KWh"].count()
-test_value = int(c - (0.9*c)) 
-a = a[["KWh","index"]]
+dd = pd.read_csv("powernetworks.csv")
 
 
 # In[ ]:
@@ -59,57 +25,128 @@ a = a[["KWh","index"]]
 
 
 
-# In[10]:
+# In[3]:
 
 
-X = a['KWh']
-y = a['index']
-    
-X_train = X[X.index < 35 ]
-y_train = y[y.index < 35 ]              
-    
-X_test = X[X.index >= 35 ]    
-y_test = y[y.index >= 35  ]
-X
+#dd = dd[:80000]
+count = dd["LCLid"].count()
+mac = dd.LCLid.unique()
+dd['KWh'] = dd['KWh'].astype(float)
+macid = []
+i=0
+for x in mac:
+    macid.append(i) 
+    i=i+1  
+i=int(i)
+dd["time"]=0
+i
 
 
-# In[19]:
+# In[12]:
 
 
-# build our RF model
-RF_Model = RandomForestRegressor(n_estimators=1000,
-                                 max_features=1, oob_score=True)
+t= pd.timedelta_range(start='1 days',periods = count , freq='H')
+time = pd.Series(range(len(t)), index=t)
 
-# let's get the labels and features in order to run our 
-# model fitting
-labels = y_train#[:, None]
-features = X_train[:, None]
+dd['time'] = time
+dd["time"] = t
+#dd['LCLid'] = df.LCLid
+#dd['KWh'] = df.KWh
+#dd['time'] = pd.to_datetime(dd["time"])
 
-# Fit the RF model with features and labels.
-rgr=RF_Model.fit(features, labels)
 
-# Now that we've run our models and fit it, let's create
-# dataframes to look at the results
-X_test_predict=pd.DataFrame(
-    rgr.predict(X_test[:, None])).rename(
-    columns={0:'KWh'}).set_index('KWh')
-X_train_predict=pd.DataFrame(
-    rgr.predict(X_train[:, None])).rename(
-    columns={0:'KWh'}).set_index('KWh')
+# In[14]:
 
-# combine the training and testing dataframes to visualize
-# and compare.
-RF_predict = X_train_predict.append(X_test_predict)
+
+
+dd['hours'] = t
+dd['date'] = 0
+#dd['DateTime'] = pd.to_datetime(df['DateTime'])
+#dd['date'], df['hours'] = df['time'].dt.normalize(), df['time'].dt.time
+dd
+
+
+# In[15]:
+
+
+values = []
+
+for x in range(i):
+        ids = dd.groupby(by = "LCLid").get_group(mac[x])
+        ids = ids[['KWh','time']]
+        values.append(ids)
+        macid.append(ids) 
+house = values[2]
+
+
+# In[16]:
+
+
+house.plot()
 
 
 # In[17]:
 
 
-a[['KWh','index']].plot()
+from statsmodels.graphics.tsaplots import plot_acf
+plot_acf(house["KWh"])
+
+
+# In[18]:
+
+
+x = house["KWh"]
+x = np.array(x).reshape((-1, 1))
+y = house['time']
+y= np.array(y).reshape((-1, 1))
+
+
+# In[19]:
+
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+# In[20]:
+
+
+model = LinearRegression()
+
+
+# In[21]:
+
+
+model.fit(x,y)
+
+
+# In[23]:
+
+
+y_predicted = model.predict(x)
+y_predicted
 
 
 # In[ ]:
 
 
+rmse = mean_squared_error(y, y_predicted)
+r2 = r2_score(y, y_predicted)
 
+# printing values
+print('Slope:' ,regression_model.coef_)
+print('Intercept:', regression_model.intercept_)
+print('Root mean squared error: ', rmse)
+print('R2 score: ', r2)
+
+# plotting values
+
+# data points
+plt.scatter(x, y, s=10)
+plt.xlabel('x')
+plt.ylabel('y')
+
+# predicted values
+plt.plot(x, y_predicted, color='r')
+plt.show()
 
